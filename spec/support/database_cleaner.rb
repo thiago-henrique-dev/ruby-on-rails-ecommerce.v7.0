a5ac7385433b249a2a1cd596469868a38fd4ad19
thiagohenriquedev <thiagohenriquedev>
2022-10-23 23:28:57 -0300

    Configurando o database Cleaner

---
+++ spec/support/database_cleaner.rb
@@ -0,0 +1,17 @@
+Rspec.config do |config| 
+    config.before(:suite) do
+        DatabaseCleaner.clean_with(:truncation)
+  end
+    config.before(:each) do
+        DatabaseCleaner.strategy = :transaction
+  end
+    config.before(:each, js: true) do
+        DatabaseCleaner.strategy = :transaction
+  end
+    config.before(:each) do
+        DatabaseCleaner.start
+ end
+    config.before(:each) do
+        DatabaseCleaner.clean
+ end
+end
