3ccb46941b8ed0cd6ce353d4fbb8cb7625ef4664
thiagohenriquedev <thiagohenriquedev>
2022-11-22 17:51:58 -0300

    Finalizando o CRUD de categorias

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
-RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe 'Category', type: :model do
+
+    context "Espero validar todos os campos obrigatorios" do
+
+    it 'Validar o campo nome está presente' do
+        category = build(:category)
+
+        expect(category.valid?).to eq(true)  
+
+    end
+
+    it 'Validar o campo nome está presente' do
+      category = build(:category, name: nil)
+
+      expect(category.valid?).to eq(false)  
+    end
+
+    it 'Validar o campo nome está presente' do
+      category = build(:category, position: nil)
+
+      expect(category.valid?).to eq(false)  
+    end
+  end
 end
+              
