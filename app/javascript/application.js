8c3c3bfa2cdb3ea200605647651c4b33674b14f0
thiagohenriquedev <thiagohenriquedev>
2022-11-22 09:35:26 -0300

    Criando o teamplate do Admin

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,4 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
-import './sbadmin/js/sb-admin-2'
-import './sbadmin/js/demo'
 import * as bootstrap from "bootstrap"
